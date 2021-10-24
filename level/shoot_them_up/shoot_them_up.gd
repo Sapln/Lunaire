8f07e29824fd758c0cfc4431dbdb0c8b1661f594
citorva <duet.venceslas@orange.fr>
2021-10-24 12:21:21 +0200

    Using background

--- level/shoot_them_up/shoot_them_up.gd
+++ level/shoot_them_up/shoot_them_up.gd
@@ -1,29 +1,89 @@
 extends Node2D
 
-var asteroid = preload("res://level/shoot_them_up/elements/asteroid.tscn")
+const Asteroid = preload("res://level/shoot_them_up/elements/asteroid.tscn")
+
+const SPACESHIP_SPEED = 300
+const SPAWN_DELAY = 0.5
+
+const LEVEL_LENGTH = 180
+
+
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var last_spawn = 0
+var timer = 0
+var background = null
 
+func prepare_element(element, vrect, spaceship_speed):	
+	var node = element.instance()
+	
+	var radius = node.gen_randomely(randi())
+	
+	node.linear_damp = 0
+	
+	node.linear_velocity = Vector2(
+		rand_range(-50, 50),
+		rand_range(-1, 1) + spaceship_speed
+	)
+	
+	node.position.y = vrect.position.y - radius
+	
+	if node.linear_velocity.x > 0:
+		node.position.x = rand_range(
+			vrect.position.x,
+			vrect.position.x + vrect.size.x/2
+		)
+	else:
+		node.position.x = rand_range(
+			vrect.position.x + vrect.size.x/2,
+			vrect.position.x + vrect.size.x
+		)
+	
+	return node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	for i in range(100):
-		var node = asteroid.instance()
-		
-		node.position.x = rand_range(32.0, 992.0)
-		node.position.y = rand_range(32.0, 568.0)
-		
-		node.linear_velocity = Vector2(
-			rand_range(-100, 100),
-			rand_range(-10, 10)
-		)
-		
-		add_child(node)
-
+	
+	var vrect = get_viewport_rect()
+	var texture = GradientTexture.new()
+	
+	background = get_node("Background")
+	background.offset.y = -1
+	background.offset.x = 0
+	background.centered = false
+	background.rotation_degrees = 90
+	background.scale.x = 1
+	
+	texture.gradient = Gradient.new()
+	
+	texture.gradient.add_point(0, Color.black)
+	texture.gradient.add_point(1, Color.darkblue)
+	
+	background.texture = texture
+	
+	update_background_geometry(vrect)
+	
+func update_background_geometry(vrect):
+	background.position = vrect.position
+	background.scale.y = vrect.size.x
+	
+	background.texture.width = vrect.size.y
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	last_spawn += delta
+	timer += delta
+	
+	var vrect = get_viewport_rect()
+	
+	update_background_geometry(vrect)
+	
+	if last_spawn >= SPAWN_DELAY:
+		last_spawn = 0
+		add_child(
+			prepare_element(Asteroid, vrect, SPACESHIP_SPEED)
+		)
