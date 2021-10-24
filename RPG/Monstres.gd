fc0c7f00408e9c8a2d435e7e82920faf777b242b
MarianPoirot <81marianp@gmail.com>
2021-10-24 12:54:12 +0200

    Poyo !

--- RPG/Monstres.gd
+++ RPG/Monstres.gd
@@ -5,10 +5,12 @@ export var max_speed = 250  # Maximum speed range.
 
 func _ready():
 	var mob_types = $AnimatedSprite.frames.get_animation_names()
-	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
-
+	#$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	$AnimatedSprite.animation = "voler"
 
 func _on_VisibilityNotifier2D_screen_exited():
+	$KirboDie.play()
+	yield(get_tree().create_timer(1),"timeout")
 	queue_free()
 
 func handle_hit():
