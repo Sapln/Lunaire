5aa387bf5706ff63735c51e35dcdf3f6d30de5e4
Sapln <59568272+Sapln@users.noreply.github.com>
2021-10-24 12:20:22 +0200

    Dinette quasi complete

--- level/dinette/dinette.gd
+++ level/dinette/dinette.gd
@@ -1,8 +1,9 @@
 extends Node2D
-enum {CAROTTE,CHAMPI,CITRON,COURGETTE,CUISSE,FROMAGE,LAIT,OEUF,OIGNON,ORANGE,POISSON,SALADE,TOMATE,VIANDE}
+enum {CAROTTE = 1,CHAMPI,CITRON,COURGETTE,CUISSE,FROMAGE,LAIT,OEUF,OIGNON,ORANGE,POISSON,SALADE,TOMATE,VIANDE,FARINE,SPAGHET}
 
-var recette = [0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var recette = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 var is_Fridge_Open = false;
+var is_Placard_Open = false;
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -44,17 +45,23 @@ func add_Aliment(alim):
 		path = "res://ressources/dinette/tomate.png"
 	elif (alim ==13):
 		path = "res://ressources/dinette/viande.png"
+	elif (alim ==14):
+		path = "res://ressources/dinette/farine.png"
+	elif (alim ==15):
+		path = "res://ressources/dinette/spaghetti.png"
 		
 	sprite.texture = load(path)
-	sprite.position.x = 400
-	sprite.position.y = 300
-	$ingredients.add_child(sprite)
+	$assietteIngredient.add_child(sprite)
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 func change_Fridge_State():
 	is_Fridge_Open = !is_Fridge_Open;
+	pass;
+	
+func change_Placard_State():
+	is_Placard_Open = !is_Placard_Open;
 	pass;
 
 
@@ -66,8 +73,8 @@ func _on_poele_input_event(viewport, event, shape_idx):
 		#	print(i);
 		plat_cuisine(recette)
 		#reset
-		recette = [0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-		var node = get_node("ingredients")
+		recette = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+		var node = get_node("assietteIngredient")
 		for i in node.get_children():
 			node.remove_child(i)
 			i.queue_free()
@@ -78,11 +85,39 @@ func plat_cuisine(recette):
 	var res = 1
 	for i in range(len(recette)):
 		if(recette[i] != 0):
-			res *= recette[i]*i
+			res *= recette[i]*(i+1)
 	
-	if(res == OEUF*2*CHAMPI*FROMAGE):
-		print("omelette")
+	var node = get_node("assiettePlat")
+	for i in node.get_children():
+		node.remove_child(i)
+		i.queue_free()
+	
+	var sprite = Sprite.new()
+	var path = String()
+	
+	if(res == OEUF*CHAMPI*FROMAGE):
+		path = "res://ressources/dinette/omelette.png"
 	elif(res == SALADE*TOMATE*CAROTTE or res == SALADE*TOMATE*OEUF):
-		print("salade composee")
+		path = "res://ressources/dinette/saladeComp.png"
+	elif(res == OEUF*LAIT*FARINE):
+		path = "res://ressources/dinette/crepe.png"
+	elif(res == SPAGHET*VIANDE*TOMATE):
+		path = "res://ressources/dinette/bolognaise.png"
+	elif(res == POISSON*CITRON):
+		path = "res://ressources/dinette/poissonAss.png"
+	elif(res == VIANDE*OIGNON):
+		path = "res://ressources/dinette/viandeAss.png"
+	elif(res == COURGETTE*TOMATE):
+		path = "res://ressources/dinette/ratatouille.png"
+	elif(res == CUISSE*VIANDE*POISSON):
+		path = "res://ressources/dinette/assortimentViandes.png"
+	elif(res == ORANGE*CITRON):
+		path = "res://ressources/dinette/jus.png"
+	else:
+		path = "res://ressources/dinette/caca.png"
+
+	sprite.texture = load(path)
+	$assiettePlat.add_child(sprite)
 	pass
+
 
