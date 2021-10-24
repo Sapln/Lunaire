05b9c5649d899cc0ac260f20316e4d59fe7292e6
citorva <duet.venceslas@orange.fr>
2021-10-24 12:20:42 +0200

    Use directely SVG file

--- level/shoot_them_up/elements/asteroid.gd
+++ level/shoot_them_up/elements/asteroid.gd
@@ -2,24 +2,28 @@ extends RigidBody2D
 
 const SPEED_MIN = 0.01 # In °/s
 const SPEED_MAX = 50.0 # In °/s
-var speed = rand_range(SPEED_MIN, SPEED_MAX)
+var speed = 0
 
 const SPRITE_NUM = 3 # asteroid_1/asteroid_2/asteroid_3
 const SPRITE_PREFIX = "res://ressources/shoot_them_up/asteroid_"
 
+var generated = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():	
+	if not generated:
+		gen_randomely(randi())
+
+func gen_randomely(rnd):
 	var image = Image.new()
 	var texture = ImageTexture.new()
 	var shape = CircleShape2D.new()
-	
-	linear_damp = 0
 		
 	image.load(
 		SPRITE_PREFIX + str(
 			# A randomly generated ID between 1 and SPRITE_NUM included
-			(randi() % SPRITE_NUM) + 1
-		) + ".png"
+			(rnd % SPRITE_NUM) + 1
+		) + ".svg"
 	)
 	texture.create_from_image(image)
 	
@@ -27,7 +31,10 @@ func _ready():
 	shape.radius = image.get_width()/2
 	
 	get_node("CollisionShape2D").shape = shape
-
+	
+	speed = rand_range(SPEED_MIN, SPEED_MAX)
+	
+	return shape.radius
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
